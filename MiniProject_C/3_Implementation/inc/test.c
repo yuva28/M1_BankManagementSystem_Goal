ec0a3f24f85902d7dae651309fa04f5cb0a302a1
yuva28 <94280220+yuva28@users.noreply.github.com>
2021-11-24 09:01:42 +0530

    Add files via upload

---
+++ MiniProject_C/3_Implementation/inc/test.c
@@ -0,0 +1,22 @@
+#include "unity/unity.h"
+
+/*required by the unity test framework*/
+void setUp()
+{
+
+}
+void tearDown()
+{
+
+}
+
+
+int main(void)
+{   
+    /*required by the unity test framework*/
+    UNITY_BEGIN();
+    /*required by the unity test framework*/
+    
+    /*required by the unity test framework*/
+    return UNITY_END();
+}
